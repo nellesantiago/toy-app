25dd2fd164254fe83f498eeedd56dfda6f521d1e
Maia <maia@Maias-MacBook-Air.local>
2023-02-24 13:23:09 +0800

    add model test for user

---
+++ test/models/user_test.rb
@@ -0,0 +1,61 @@
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(first_name: "Rails", last_name: "Test", email: "rails@email.com", password: "test123")
+  end
+
+  test "user should be valid" do
+    assert @user.save
+  end
+
+  test "user should have a first name" do
+    @user.first_name = nil
+    assert_not @user.save
+  end
+
+  test "user should have a last name" do
+    @user.last_name = nil
+    assert_not @user.save
+  end
+
+  test "should have a valid first name" do
+    @user.first_name = "Rails123"
+    assert_not @user.save
+  end
+
+  test "user should have a valid last name" do
+    @user.last_name = "Test456"
+    assert_not @user.save
+  end
+
+  test "user should have an email" do
+    @user.email = nil
+    assert_not @user.save
+  end
+
+  test "user should have a valid email" do
+    @user.email = "invalid email"
+    assert_not @user.save
+  end
+
+  test "user should have a unique email" do
+    @user.save
+    @user2 = User.new(first_name: "Rails", last_name: "Test", email: @user.email, password: "test123")
+    assert_not @user2.save
+  end
+
+  test "user should have a password" do
+    @user.password = nil
+    assert_not @user.save
+  end
+
+  test "user should have a valid password" do
+    @user.password = "test"
+    assert_not @user.save
+
+    @user.password = "test123"
+    assert @user.save
+  end
+
+end
