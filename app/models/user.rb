7a992a0b3885d316f6dd070a38481a3d4c3350b6
Maia <maia@Maias-MacBook-Air.local>
2023-02-26 10:52:59 +0800

    add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,22 @@
 class User < ApplicationRecord
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+    BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
   has_secure_password
 
   validates_presence_of :first_name, :last_name, :email, :password
 
-  before_save { self.email = email.downcase }
-
-  validates :email, uniqueness: true
   validates :first_name, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, length: { in: 1..20 }
   validates :last_name, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, length: { in: 1..20 }
+
+  validates :email, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  before_save { self.email = email.downcase }
+
   validates :password, length: { in: 6..20 }
 
   enum role: { user: 0, admin: 1 }
-
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-      BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
 end
