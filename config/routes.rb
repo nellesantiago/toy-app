921f31818f3666240c07b0087416eb8b92232689
Maia <maia@Maias-MacBook-Air.local>
2023-02-27 18:06:40 +0800

    add user routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # root "users#index"
+  root "sessions#new"
+
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+
+  get "signup", to: "users#new"
+
+  resources :users, except: :new
+  resources :posts
 end
+
+
