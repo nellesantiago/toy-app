b875d00fa1d977cef6cb6dfde85de4ee6ed10748
Maia <maia@Maias-MacBook-Air.local>
2023-03-04 01:13:23 +0800

    add home page controller to separate my login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   # root "articles#index"
   # root "users#index"
-  root "sessions#new"
-
+  root "home_page#index"
+  
+  get 'home_page/index'
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
